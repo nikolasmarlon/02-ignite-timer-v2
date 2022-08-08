7305aaecece3dab866ed687ab154128141af28f6
Nikolas Marlon <40303291+nikolasmarlon@users.noreply.github.com>
2022-08-08 11:09:14 -0300

    instalação styled-components
    instalação e configuração de styled-components e @types

---
+++ src/@types/styles.d.ts
@@ -0,0 +1,7 @@
+//definição de tipos do typeScript
+
+import 'styled-components'
+import { defaultTheme } from '../styles/themes/default'
+
+
+type ThemeType = typeof defaultTheme
--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
+import { ThemeProvider } from 'styled-components' 
 import { useState } from 'react'
+import { defaultTheme } from './styles/themes/default'
 
 
 export function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div>
+    <ThemeProvider theme={defaultTheme}>
       <h1>Alô Mundo!</h1>
-    </div>
+    </ThemeProvider>
   )
 }
 
